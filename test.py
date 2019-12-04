7c0491da672e65a7c9cf53692e4bdf5b82741499
mayn <460132066@qq.com>
2019-12-04 22:04:46 +0800

    test

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-import numpy
-print("test")
+import ctypes
+import os
+CUR_PATH=os.path.dirname(__file__)
+# dllPath=os.path.join(CUR_PATH,"pycall.dll")
+# print (dllPath)
+#mydll=ctypes.cdll.LoadLibrary(dllPath)
+#print mydll
+pDll=ctypes.WinDLL("C:\\Users\\mayn\\PycharmProjects\\untitled2\\pycall.dll")
+print (pDll)
+
+pResutl= pDll.sum(1,4)
+pResult2=pDll.sub(1,4)
+print (pResutl)
+print (pResult2)
